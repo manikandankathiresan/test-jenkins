80f0ee0a73a26e7efad3d9de34d68e28fb83e1bb
Manikandan <manikandan.k@mitrahsoft.com>
2024-12-13 19:48:15 +0530

    removed route

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello, World! This is a Node.js app inside Docker.');
 });
 
-app.get('/hello', (req, res) => {
-  res.send('Hello, World! This is a Node.js app inside Docker.');
-});
 
 // If you're using the app with a test, export it for use in the test file
 if (process.env.NODE_ENV !== 'test') {
